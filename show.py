9e62a38e27a7cfc0a8622d8ea5504ef465d69eae
ppmar <omar.amine.pro@gmail.com>
2025-09-08 16:37:59 +0200

    documentation show.py

--- show.py
+++ show.py
@@ -1,6 +1,8 @@
-format = "id;description"
-
 def show(file_path):
+    """
+    Displays tasks from a file in a formatted manner.
+    Each line in the file should be in the format: id;description
+    """
     with open(file_path, 'r') as file:
         lines = file.readlines()
         for line in lines:
