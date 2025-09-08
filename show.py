8a989cccca2d85598d1e12f3a5dd7eda4a506f4c
ppmar <omar.amine.pro@gmail.com>
2025-09-08 16:32:53 +0200

    correction show.py

--- show.py
+++ show.py
@@ -1,4 +1,11 @@
-def show(Tasks):
-    for task in Tasks:
-        print(f"Task ID: {task['id']}, Description: {task['description']}")
+format = "id;description"
+
+def show(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            print(f"{line.strip().split(';')[0]}: {line.strip().split(';')[1]}")
+
+show("test.csv")
+            
               
