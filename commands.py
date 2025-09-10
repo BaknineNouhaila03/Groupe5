290bd0353552aa2018a542b91b3f19661d35e8ef
unknown <bakninenouha@gmail.com>
2025-09-10 15:52:07 +0200

    modifications

--- commands.py
+++ commands.py
@@ -2,18 +2,20 @@ from task import Task
 from utils.print import print_tasks
 
 
-def add(tasklist: dict[int, Task], description: str):
+def add(tasklist: dict[int, Task], description: str , etat: str):
     task_id = max((tasklist.keys()), default=0) + 1
-    tasklist[task_id] = Task(str(task_id), description)
+    tasklist[task_id] = Task(str(task_id), description , etat)
     print("Task added with ID:", task_id)
 
 
-def modify(tasklist: dict[int, Task], task_id: int, description: str):
+def modify(tasklist: dict[int, Task], task_id: int, description = None, etat = None):
     try:
-        tasklist[task_id].description = description
+        if description != None : 
+         tasklist[task_id].description = description
+        elif etat != None :
+         tasklist[task_id].etat = etat
     except KeyError:
         print(f"Task with ID {task_id} not found.")
-
 
 def rm(tasklist: dict[int, Task], task_id: int):
     try:
--- task.py
+++ task.py
@@ -1,4 +1,5 @@
 class Task:
-    def __init__(self, task_id: str, description: str):
+    def __init__(self, task_id: str, description: str, etat: str):
         self.id: str = task_id
         self.description: str = description
+        self.etat :str = etat
