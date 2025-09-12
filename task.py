74a40ca5dc07204f4892a22b78f1b0fa4435ea5b
C8H10O2 <xiaoyu.tang.fr@gmail.com>
2025-09-12 10:08:07 +0200

    add started_at and interval

--- task.py
+++ task.py
@@ -1,5 +1,11 @@
+from datetime import datetime, timedelta
+
+
 class Task:
-    def __init__(self, task_id: str, description: str, state: str):
+    def __init__(self, task_id: str, description: str, state: str,
+                 started_at: datetime, interval: timedelta ):
         self.id: str = task_id
         self.description: str = description
         self.state :str = state
+        self.started_at = started_at
+        self.interval = interval
