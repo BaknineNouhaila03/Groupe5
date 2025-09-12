e83d2d0558864cff0d9ed712b9d3c5d029993180
unknown <bakninenouha@gmail.com>
2025-09-12 10:20:33 +0200

    changing interval to recurrence

--- task.py
+++ task.py
@@ -3,9 +3,9 @@ from datetime import datetime, timedelta
 
 class Task:
     def __init__(self, task_id: str, description: str, state: str,
-                 started_at: datetime, interval: timedelta ):
+                 started_at: datetime, recurrence: str ):
         self.id: str = task_id
         self.description: str = description
         self.state :str = state
         self.started_at = started_at
-        self.interval = interval
+        self.recurrence :str = recurrence
